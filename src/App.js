b9052dd0c5b211d9c4da9d222bdb94c15138921c
Vialoha <vi62469@gmail.com>
2023-08-17 11:24:56 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+//import dataCountries from '../src/countries.json'
+import React, {useState, useEffect} from 'react';
+
+import Navbar from '../src/components/Navbar';
+
+import CountriesList from '../src/components/CountriesList';
+
+import CountryDetails from '../src/components/CountryDetails';
+
+import { Routes, Route } from 'react-router-dom';
+
+import axios from 'axios';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  // call is made inside the useEffect hook with an empty dependency array [], 
+  // which ensures that the effect runs only once after the initial render.
+  useEffect(() => {
+    axios.get ('https://ih-countries-api.herokuapp.com/countries')
+      .then (response => {
+        setCountries(response.data);
+        console.log(response.data);
+      })
+      .catch (error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+    
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="countriesList">
+        <CountriesList countries={countries}/>
+      </div>
+
+      <div className="countriesDetails">
+      <Routes>
+          <Route
+            path="/countries/:alpha3Code"
+            element={ <CountryDetails/>} 
+          />
+        </Routes>
+      </div>
+
     </div>
   );
 }
